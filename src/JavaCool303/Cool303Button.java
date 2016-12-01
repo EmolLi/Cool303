c4996f44b564f60586342eddfdb9f419c17c6ad0
Watchandmeet <watchandmeet@gmail.com>
2016-12-01 14:39:05 -0500

    lol

---
+++ src/JavaCool303/Cool303Button.java
@@ -0,0 +1,18 @@
+package JavaCool303;
+import javax.swing.*;
+
+
+public class Cool303Button extends Cool303Component {
+
+	private JButton btn;
+	
+	/**
+	 * constructor
+	 */
+	public Cool303Button(){
+		this.btn = new JButton();
+		 
+	}
+	
+	
+}

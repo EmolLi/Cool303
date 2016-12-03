b9bd46c4a608421b88b1568695d1c125c61b4d1b
Emol <duan.li@mail.mcgill.ca>
2016-12-03 17:25:44 -0500

    Add files via upload

---
+++ Cool303ThemePastel.java
@@ -0,0 +1,23 @@
+package JavaCool303;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+
+public class Cool303ThemePastel extends Theme {
+	
+	public Cool303ThemePastel(){
+		buttonTextColor = new Color(100,100,100);
+		buttonBackground = new Color(100,100,200);
+//		private int buttonFontSize;
+		buttonDimension = new Dimension(100,200); 
+		buttonFont = new Font("Courier New", Font.ITALIC, 12);
+		buttonIcon = null;
+
+		
+
+		
+	}
+}
